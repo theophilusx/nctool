6221663019d4b65404fca920cb3721be0a41abc5
Tim Cross <theophilusx@gmail.com>
2020-08-21 04:08:09 +1000

    Move calcOffset to utils namespace

--- src/index.js
+++ src/index.js
@@ -49,25 +49,6 @@ const parseArgs = () => {
   return args;
 };
 
-// this needs to be generalised to handle n-dimensional arrays!
-const calcOffset = (indicies, dimensions) => {
-  let nDims = dimensions.length;
-
-  switch (nDims) {
-    case 1:
-      return indicies[0];
-    case 2:
-      return indicies[1] + dimensions[1] * indicies[0];
-    case 3:
-      return (
-        indicies[2] +
-        dimensions[2] * (indicies[1] + dimensions[1] * indicies[0])
-      );
-    default:
-      return -1;
-  }
-};
-
 const main = async () => {
   let nc;
 
